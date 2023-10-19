de6b6f134af7a7762217d180efdb294c293d936b
Abhijeet <abhijeetmaharana77@gmail.com>
2023-10-19 20:49:32 +0530

    Requirements

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r",encoding='utf-8') as f:
+    long_description = f.read()
+    
+    
+__version__ = '0.0.1'
+
+REPO_NAME = 'Text-Summarization'
+AUTHOR_USER_NAME = 'abhi227070'
+SRC_REPO = 'Text-Summarization'
+AUTHOR_EMAIL = 'abhijeetmaharana77@gmail.com'
+
+
+setuptools.setup(
+    
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarization tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_url = {
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+    
+)
